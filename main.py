ae53ee8bf05f750d2a471a97c1ae72715f47a97c
Nyambevos <ruslan.bilokoniuk@gmail.com>
2024-05-19 17:50:11 +0300

    add home page

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from starlette.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from src.routes import auth, users, admin
 
 app = FastAPI()
+templates = Jinja2Templates(directory="src/templates")
 
 app.include_router(auth.router, prefix='/api')
 app.include_router(users.router, prefix='/api')
 app.include_router(admin.router, prefix='/api')
 
-@app.get("/")
-def read_root():
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
     """
     The read_root function is a function that returns the string &quot;Hello World&quot;
     in JSON format. This is an example of how to use FastAPI to create a ReST API.
@@ -18,7 +22,7 @@ def read_root():
     :return: A dictionary with a key &quot;message&quot; and
     :doc-author: Trelent
     """
-    return {"message": "Hello World"}
+    return templates.TemplateResponse("index.html", {"request": request})
 
 if __name__ == "__main__":
     uvicorn.run('main:app', host="localhost", port=8000, reload=True)
