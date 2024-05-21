25b6ddfcb7bd6731b780307f6214c31a6ae44c55
Nyambevos <ruslan.bilokoniuk@gmail.com>
2024-05-21 20:53:50 +0300

    fixed image

--- src/routes/images.py
+++ src/routes/images.py
@@ -1,27 +1,33 @@
-from pathlib import Path
-import shutil
-from fastapi import FastAPI, File, UploadFile, APIRouter
+from fastapi import Depends, File, UploadFile, APIRouter
 from fastapi.responses import FileResponse, HTMLResponse
-from utils import apply_effect, round_corners, crop_image
+from sqlalchemy.orm import Session
+from typing import List
+
+from src.database.models import User
+from src.database.db import get_db
+from src.repository import images as repository_images
+from src.services.auth import auth_service
+from src.routes.utils import apply_effect, round_corners, crop_image
 
 router = APIRouter(prefix='/images', tags=["images"])
 
-@router.post("/upload_image/")
-async def upload_image(file: UploadFile = File(...)):
-    folder = Path('src/server')
+@router.post("/upload")
+async def upload_file(description: str, hashtags: List[str], db: Session = Depends(get_db), current_user: User = Depends(auth_service.get_current_user),   file: UploadFile = File(...)):
+    return await repository_images.create_images_post(description, hashtags, current_user, db,  file)
 
-    image_path = folder / file.filename
+@router.get("/get_image")
+async def get_image(db: Session = Depends(get_db), current_user: User = Depends(auth_service.get_current_user)):
+    return await repository_images.get_images(current_user, db)
 
-    with image_path.open("wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
+@router.delete("/delete_image")
+async def delete_image(image_id: str , db: Session = Depends(get_db), current_user: User = Depends(auth_service.get_current_user)):
+    return await repository_images.del_images(image_id, db, current_user)
 
-    return {"message": f"Image '{file.filename}' uploaded successfully!"}
-
-
-@router.get("/images", response_class=HTMLResponse)
-async def read_image(image_id: str):
-    img_path = f"src/server/{image_id}.jpg"
-    return FileResponse(img_path, media_type="image/jpeg")
+@router.put("/edit_description_image")
+async def delete_image(image_id: str , new_description: str, 
+                       db: Session = Depends(get_db), 
+                       current_user: User = Depends(auth_service.get_current_user)):
+    return await repository_images.put_images(image_id, new_description, current_user, db)
 
 @router.get("/images/crop")
 async def crop_image_view(image_id: str, width: int, height: int):
