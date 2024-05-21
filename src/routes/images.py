9741fe29f1e4ea5cc49eb7fe81c9fc6e313a1a9f
Nyambevos <ruslan.bilokoniuk@gmail.com>
2024-05-21 22:43:16 +0300

    fixed

--- src/routes/images.py
+++ src/routes/images.py
@@ -88,4 +88,46 @@ async def put_image(image_id: int , new_description: str,
     """
     return await repository_images.put_image(image_id, new_description, current_user, db)
     
-    
+@router.get("/images/crop")
+async def crop_image_view(image_id: str, width: int, height: int):
+   """
+   Обрізає зображення за заданими шириною та висотою.
+   Args:
+       image_id: Ідентифікатор зображення.
+       width: Ширина обрізаного зображення.
+       height: Висота обрізаного зображення.
+   Returns:
+       JSON-відповідь з URL-адресою обрізаного зображення.
+   """
+   image_path = f"src/server/{image_id}.jpg"
+   manipulated_url = await crop_image(image_path, width=width, height=height)
+   return {"url": manipulated_url}
+
+@router.get("/images/effect")
+async def apply_effect_view( image_id: str, effect: str):
+    """
+    Застосовує ефект до зображення.
+    Args:
+        image_id: Ідентифікатор зображення.
+        effect: Назва ефекту (наприклад, "sepia", "vignette").
+        **kwargs: Додаткові аргументи для ефекту.
+    Returns:
+        JSON-відповідь з URL-адресою зображення з ефектом.
+    """
+    image_path = f"src/server/{image_id}.jpg"
+    manipulated_url = await apply_effect(image_path, effect=effect)
+    return {"url": manipulated_url}
+
+@router.get("/images/roundcorners")
+async def round_corners(image_id: str, radius: int):
+    """
+    Закругляє кути.
+    Args:
+        image_id: Ідентифікатор зображення.
+        radius: Радіус зрізання кутів.
+    Returns:
+        JSON-відповідь з URL-адресою зображення без фону.
+    """
+    image_path = f"src/server/{image_id}.jpg"
+    manipulated_url = await round_corners(image_path, "removebg")
+    return {"url": manipulated_url}
