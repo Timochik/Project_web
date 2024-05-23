2645345f21d7b0fdaa86ba428d32da5d5372f9cf
andrii-trebukh <andrew.trebukh@gmail.com>
2024-05-23 18:29:51 +0200

    added tests for change and get comments

--- tests/test_route_comments.py
+++ tests/test_route_comments.py
@@ -1,4 +1,6 @@
-from src.database.models import Post
+import asyncio
+from src.database.models import Post, User, UserRole, Comments
+from src.services.auth import auth_service
 
 
 def test_post_comment_image_not_found(client, comment, get_token):
@@ -12,14 +14,13 @@ def test_post_comment_image_not_found(client, comment, get_token):
     assert response.status_code == 404, response.text
     data = response.json()
     assert data["detail"] == "Image not found"
-    
 
 
 def test_post_comment(client, comment, get_token, session):
     test_image = Post(
-        description = "test_description",
-        image_url = "http://test_url.com",
-        author_id = 1
+        description="test_description",
+        image_url="http://test_url.com",
+        author_id=1
     )
     session.add(test_image)
     session.commit()
@@ -38,4 +39,114 @@ def test_post_comment(client, comment, get_token, session):
     assert "id" in data
     assert "created_at" in data
     assert data["updated_at"] is None
+
+
+def test_change_comment(client, get_token):
+    token = get_token
+    headers = {"Authorization": f"Bearer {token}"}
+    body = {
+        "comment_id": 1,
+        "new_text": "New_text"
+    }
+    response = client.put(
+        "/api/comments",
+        headers=headers,
+        json=body,
+    )
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["id"] == body.get("comment_id")
+    assert data["text"] == body.get("new_text")
+    assert "created_at" in data
+    assert data["updated_at"] is not None
+
+
+def test_change_comment_not_found(client, get_token):
+    token = get_token
+    headers = {"Authorization": f"Bearer {token}"}
+    body = {
+        "comment_id": 2,
+        "new_text": "New_text"
+    }
+    response = client.put(
+        "/api/comments",
+        headers=headers,
+        json=body,
+    )
+    assert response.status_code == 404, response.text
+    data = response.json()
+    assert data["detail"] == "Comment not found"
+
+
+def test_change_comment_access_denied(client, session):
+    second_test_user = User(
+        username="username",
+        email="email@example.com",
+        password="password",
+        role=UserRole.user
+    )
+    session.add(second_test_user)
+    session.commit()
+
+    token = asyncio.run(
+        auth_service.create_access_token(
+            data={"sub": second_test_user.email}
+        )
+    )
+    headers = {"Authorization": f"Bearer {token}"}
+    body = {
+        "comment_id": 1,
+        "new_text": "New_text_from_invalid_user"
+    }
+    response = client.put(
+        "/api/comments",
+        headers=headers,
+        json=body,
+    )
+    assert response.status_code == 403, response.text
+    data = response.json()
+    assert data["detail"] == "Access denied"
+
+
+def test_get_comment(client, session):
+    comment_id = 1
+    current_comment: Comments = session.query(Comments).filter(
+        Comments.id == comment_id
+    ).first()
+    response = client.get(f"/api/comments/{comment_id}")
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["id"] == current_comment.id
+    assert data["text"] == current_comment.text
+    assert data["created_at"] == current_comment.created_at.isoformat()
+    assert data["updated_at"] == current_comment.updated_at.isoformat()
+    assert data["image_id"] == current_comment.image_id
+    assert data["user_id"] == current_comment.user_id
+
+
+def test_get_comment_not_found(client):
+    comment_id = 2
+    response = client.get(f"/api/comments/{comment_id}")
+    assert response.status_code == 404, response.text
+    data = response.json()
+    assert data["detail"] == "Comment not found"
+
+
+def test_get_comment_by_image(client, session):
+    image_id = 1
+    comments = session.query(Comments).filter(
+        Comments.image_id == image_id
+    ).all()
+    response = client.get(f"/api/comments/by-image/{image_id}")
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert len(data) == len(comments)
+
+
+def test_get_comment_by_image_not_found(client):
+    image_id = 2
+    response = client.get(f"/api/comments/by-image/{image_id}")
+    assert response.status_code == 404, response.text
+    data = response.json()
+    assert data["detail"] == "Image not found"
 
